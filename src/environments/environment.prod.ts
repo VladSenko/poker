a516b1bb878a5a58dee29a5ca951ef832a015a8e
vlad-senko <vlad.senko@icloud.com>
2020-04-13 00:22:48 +0300

    project init; firebase deploy setup

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBl98ApqYiibhk7n3aexRtsfo2-2FZyhiU',
+    authDomain: 'poker-188a0.firebaseapp.com',
+    databaseURL: 'https://poker-188a0.firebaseio.com',
+    projectId: 'poker-188a0',
+    storageBucket: 'poker-188a0.appspot.com',
+    messagingSenderId: '303440377149',
+    appId: '1:303440377149:web:1b2d35f7eaf2e4a4194154',
+    measurementId: 'G-3MCVQKYZZC',
+  },
+};
