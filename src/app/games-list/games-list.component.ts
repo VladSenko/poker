5a2c9e569f5edd0860a437521c7629991788c0de
vlad-senko <vlad.senko@icloud.com>
2020-04-13 20:43:11 +0300

    created user selection before navigation to game

--- src/app/games-list/games-list.component.ts
+++ src/app/games-list/games-list.component.ts
@@ -4,6 +4,9 @@ import {
     AngularFirestoreCollection,
 } from '@angular/fire/firestore';
 import { Game } from '../models/game.model';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { User } from '../models/user.model';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-games-list',
@@ -12,15 +15,43 @@ import { Game } from '../models/game.model';
 })
 export class GamesListComponent implements OnInit {
     public games: Game[];
+    public users: User[];
+    public selectedUserId: number;
 
-    constructor(private afs: AngularFirestore) {
+    private selectedGameId: string;
+
+    constructor(
+        private afs: AngularFirestore,
+        private modalService: NgbModal,
+        private router: Router
+    ) {
         this.afs
             .collection('games')
             .valueChanges()
             .subscribe((games: Game[]) => {
                 this.games = games;
             });
+
+        this.afs
+            .collection('users')
+            .valueChanges()
+            .subscribe((users: User[]) => {
+                this.users = users;
+            });
     }
 
     ngOnInit(): void {}
+
+    public openSetUserDialog(dialog: any, selectedGameId: string): void {
+        this.selectedGameId = selectedGameId;
+        this.modalService.open(dialog, { centered: true });
+    }
+
+    public navigateToGame(): void {
+        this.modalService.dismissAll();
+        if (this.selectedUserId && this.selectedGameId) {
+            localStorage.setItem('user', this.selectedUserId.toString());
+            this.router.navigate(['/game', this.selectedGameId]);
+        }
+    }
 }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
-  selector: 'app-table',
-  templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss']
+    selector: 'app-table',
+    templateUrl: './table.component.html',
+    styleUrls: ['./table.component.scss'],
 })
 export class TableComponent implements OnInit {
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
