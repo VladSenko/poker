255f80dada11c809726591f9353a8b149220fb24
vlad-senko <vlad.senko@icloud.com>
2020-04-13 21:18:40 +0300

    open game by id

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from '../models/game.model';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
-  selector: 'app-game',
-  templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+    selector: 'app-game',
+    templateUrl: './game.component.html',
+    styleUrls: ['./game.component.scss'],
 })
 export class GameComponent implements OnInit {
+    private gameId: string;
+    public game: Game;
 
-  constructor() { }
+    constructor(private route: ActivatedRoute, private afs: AngularFirestore) {
+        this.route.paramMap.subscribe(
+            (params: ParamMap) => (this.gameId = params.get('id'))
+        );
+    }
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {
+        if (this.gameId) {
+            this.afs
+                .collection<Game>('games', (ref) =>
+                    ref.where('id', '==', this.gameId)
+                )
+                .valueChanges()
+                .subscribe((games) => {
+                    this.game = games[0];
+                    console.log(this.game);
+                });
+        }
+    }
 }
